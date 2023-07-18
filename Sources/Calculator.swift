8264a1a035c59ae739c5c4878d4d2b1131b34c3e
jinyongp <dev.jinyongp@gmail.com>
2023-07-18 17:12:18 +0900

    feat: cli tool 완성

--- Sources/Calculator.swift
+++ Sources/Calculator.swift
@@ -1,26 +1,39 @@
 import Foundation
 
 class Calculator<T: Arithmetic> {
-    private var result: T
-    private var operators: [String: Calculable] = [:]
+    private var _result: T
+    private var _operators: [String: Calculable]
 
-    init(defaultValue: T = 0) {
-        self.result = defaultValue
+    init(defaultValue: T? = nil, operators: [String: Calculable] = [:]) {
+        _result = defaultValue ?? Calculator.zero()
+        _operators = operators
     }
 
+    var result: T { _result }
+
+    var operators: [String] { Array(_operators.keys) }
+
     func extend(_ name: String, _ operation: any Calculable) {
-        operators.updateValue(operation, forKey: name)
+        _operators.updateValue(operation, forKey: name)
+    }
+
+    func isSupported(_ name: String) -> Bool {
+        return _operators.keys.contains(name)
     }
 
     func clear() {
-        result = 0 as! T
+        _result = Calculator.zero()
     }
 
-    func calculate(_ rhs: T, name: String) -> T {
-        guard let operation = operators[name] else {
-            return result
-        }
+    @discardableResult
+    func calculate(_ operand: T, name: String) -> T {
+        guard let operation = _operators[name] else { return _result }
+        _result = operation.operate(_result, operand)
 
-        return operation.operate(result, rhs)
+        return _result
+    }
+
+    private static func zero() -> T {
+        return 0 as? T ?? 0.0 as! T
     }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,46 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// NAME: Calculator CLI
+// DESC: CLI tool to perform simple operations
+//
+// Created by Jinyong Park on 2023/07/17
 
-print("Hello, world!")
+typealias T = Double
+
+let calculator = Calculator<T>(
+    operators: [
+        "+": AddOperation(),
+        "-": SubOperation(),
+        "*": MulOperation(),
+        "/": DivOperation(),
+        "%": ModOperation(),
+    ]
+)
+
+printHelp(operators: calculator.operators)
+
+while true {
+    let result = String(format: "%.4f", calculator.result)
+    print("\nResult: \(result)", terminator: "\n» ")
+    guard var input = readLine() else { break }
+
+    input = String(input.filter { !" \n\t\r".contains($0) })
+    if input == "exit" { print("Goodbye."); break }
+    if input == "help" { printHelp(operators: calculator.operators); continue }
+    if input == "reset" { calculator.clear(); continue }
+
+    guard input.count >= 2 else { continue }
+    guard let sign = input.first, calculator.isSupported(String(sign)) else { continue }
+    guard let num = T(input[input.index(input.startIndex, offsetBy: 1)...]) else { continue }
+    calculator.calculate(num, name: String(sign))
+}
+
+func printHelp(operators: [String] = []) {
+    print("\n[Calculator CLI]\n")
+    print("This command provides simple arithmetic functions.\n")
+    print(" · Usage: Enter operater and operand. e.g. +42 (^C to exit)")
+    print(" · Operators: \(calculator.operators.joined(separator: ", "))")
+    print(" · Commands:")
+    print("      help        Print this message")
+    print("      reset       Reset result to zero")
+    print("      exit        Exit this program")
+    print()
+}
